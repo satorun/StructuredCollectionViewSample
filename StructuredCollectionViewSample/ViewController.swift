58a9fbea899810f67fc87c8ea06ced28efd72d70
satorun <satorun.org@gmail.com>
2025-03-03 17:12:52 +0900

    Initial Commit

---
+++ StructuredCollectionViewSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StructuredCollectionViewSample
+//
+//  Created by satoru.nishimura on 2025/03/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
