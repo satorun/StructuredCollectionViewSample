082464b7fe0927e096c6832d1e4977f3812cad42
satorun <satorun.org@gmail.com>
2025-03-03 17:32:02 +0900

    Add buildServer.json

--- StructuredCollectionViewSample/ViewController.swift
+++ StructuredCollectionViewSample/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
